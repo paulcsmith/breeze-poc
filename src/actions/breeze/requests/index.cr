03995ee77caec9a969e7e00f286756ea5c6920b9
Paul Smith <paulcsmith0218@gmail.com>
2020-03-27 14:20:32 -0400

    Add some style

--- src/actions/breeze/requests/index.cr
+++ src/actions/breeze/requests/index.cr
@@ -2,6 +2,6 @@ class Breeze::Requests::Index < BrowserAction
   include Auth::AllowGuests
 
   get "/breeze" do
-    html IndexPage, breeze_requests: BreezeRequestQuery.new
+    html IndexPage, breeze_requests: BreezeRequestQuery.new.created_at.desc_order
   end
 end
