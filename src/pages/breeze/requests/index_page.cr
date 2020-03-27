a0fe98bc7cd7c3971f16776f543c6927c663243f
Paul Smith <paulcsmith0218@gmail.com>
2020-03-27 14:32:33 -0400

    Nail Index Page

--- src/pages/breeze/requests/index_page.cr
+++ src/pages/breeze/requests/index_page.cr
@@ -22,37 +22,42 @@ class Breeze::Requests::IndexPage < BreezeLayout
   def request_row(req)
     li class: "border-t border-gray-200" do
       a class: "block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out", href: "#" do
-        div class: "flex items-center px-4 py-4 sm:px-6" do
+        div class: "flex items-center px-4 py-4 sm:px-4" do
           div class: "min-w-0 flex-1 flex items-center" do
-            div class: "min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4" do
+            div class: "min-w-0 flex-1 px-4 md:grid md:grid-cols-3 md:gap-4" do
               div do
                 div req.action, class: "text-sm leading-5 font-medium text-teal-500 truncate"
-                # div class: "mt-2 flex items-center text-sm leading-5 text-gray-500" do
-                #   span "ricardo.cooper@example.com", class: "truncate"
-                # end
               end
               div class: "hidden md:block" do
                 div do
                   div class: "text-sm leading-5 text-gray-900" do
-                    text " Happened #{time_ago_in_words(req.created_at)} ago"
+                    span class: "inline-flex items-center px-2.5 py-0.5 rounded-md text-xs font-medium leading-5 bg-gray-100 text-gray-800" do
+                      text req.method.upcase
+                    end
+                    text " #{req.path}"
                   end
-                  # div class: "mt-2 flex items-center text-sm leading-5 text-gray-500" do
-                  #   tag "svg", class: "flex-shrink-0 mr-1.5 h-5 w-5 text-green-400", fill: "currentColor", viewBox: "0 0 20 20" do
-                  #     tag "path", clip_rule: "evenodd", d: "M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z", fill_rule: "evenodd"
-                  #   end
-                  #   text " Completed phone screening "
-                  # end
+                end
+              end
+              div class: "hidden md:block" do
+                div do
+                  div class: "text-sm leading-5 text-gray-900" do
+                    text "#{time_ago_in_words(req.created_at)} ago"
+                  end
                 end
               end
             end
           end
           div do
-            tag "svg", class: "h-5 w-5 text-gray-400", fill: "currentColor", viewBox: "0 0 20 20" do
-              tag "path", clip_rule: "evenodd", d: "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z", fill_rule: "evenodd"
-            end
+            row_arrow
           end
         end
       end
+    end
+  end
+
+  def row_arrow
+    tag "svg", class: "h-5 w-5 text-gray-400", fill: "currentColor", viewBox: "0 0 20 20" do
+      tag "path", clip_rule: "evenodd", d: "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z", fill_rule: "evenodd"
     end
   end
 end
