7c6bc199b78a622bb3afe06f7a5c9b3b910f5739
Paul Smith <paulcsmith0218@gmail.com>
2020-03-27 13:01:11 -0400

    Aw yeah

--- src/pages/breeze/requests/show_page.cr
+++ src/pages/breeze/requests/show_page.cr
@@ -2,6 +2,7 @@ class Breeze::Requests::ShowPage < BreezeLayout
   needs breeze_request : BreezeRequest
 
   def content
+    link "Back to all", to: Index
     h1 "Request"
     ul do
       li @breeze_request.path
