7e76843cc11907550b3f20ad8b70b54f81cf04da
Paul Smith <paulcsmith0218@gmail.com>
2020-03-28 11:29:54 -0400

    Extract Breeze::DescriptionList component

--- src/pages/breeze/requests/show_page.cr
+++ src/pages/breeze/requests/show_page.cr
@@ -5,31 +5,29 @@ class Breeze::Requests::ShowPage < BreezeLayout
     "Request Details"
   end
 
+  def req : BreezeRequest
+    breeze_request
+  end
+
   def breadcrumb_parent
     link "All Requests", to: Index, class: "border-b-2 border-green-400 hover:text-teal-700"
     mount ArrowSvg.new("mx-1 mb-1 h-5 w-5 text-gray-400 inline-block")
   end
 
   def content
-    render_data(@breeze_request)
+    mount Breeze::DescriptionList.new(
+      heading_title: ->{ text req.action },
+      heading_subtitle: ->{ mount Breeze::Badge.new(req) },
+      list: ->{ render_list }
+    )
   end
 
-  def render_data(req)
-    mount Breeze::Panel.new do
-      div class: "px-4 py-5 border-b border-gray-200 sm:px-6" do
-        h3 req.action, class: "text-lg leading-6 font-medium text-gray-900"
-        div class: "mt-2" { mount Breeze::Badge.new(req) }
-      end
-      div class: "px-4 py-5 sm:p-0" do
-        dl do
-          render_session_info(req)
-          render_header_info(req)
-        end
-      end
-    end
+  def render_list
+    render_session_info
+    render_header_info
   end
 
-  def render_session_info(req)
+  def render_session_info
     req.session.as_h.each do |key, value|
       div class: "mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5" do
         dt class: "text-sm leading-5 font-medium text-gray-500" do
@@ -42,7 +40,7 @@ class Breeze::Requests::ShowPage < BreezeLayout
     end
   end
 
-  def render_header_info(req)
+  def render_header_info
     req.headers.as_h.each do |key, value|
       div class: "mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5" do
         dt class: "text-sm leading-5 font-medium text-gray-500" do
