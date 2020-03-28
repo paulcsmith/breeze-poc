9b254264117bad078a564cf2fbaee17b182d03bd
Paul Smith <paulcsmith0218@gmail.com>
2020-03-28 13:34:27 -0400

    Add DescriptionListRow component

--- src/components/breeze/description_list_row.cr
+++ src/components/breeze/description_list_row.cr
@@ -1,14 +1,19 @@
 class Breeze::DescriptionListRow < BaseComponent
   needs key : String
-  needs value : HtmlProc
+  needs value : HtmlProc | String
 
   def render
     div class: "mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5" do
       dt class: "text-sm leading-5 font-medium text-gray-500" do
-        text "Session #{key}"
+        text key
       end
       dd class: "mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2" do
-        text value.as_s
+        v = value
+        if v.is_a?(String)
+          text v
+        else
+          v.call
+        end
       end
     end
   end
