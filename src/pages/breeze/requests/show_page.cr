6dcc922207be2bc429e6fcda633289848bda1174
Paul Smith <paulcsmith0218@gmail.com>
2020-03-28 13:34:27 -0400

    Add DescriptionListRow component

--- src/pages/breeze/requests/show_page.cr
+++ src/pages/breeze/requests/show_page.cr
@@ -29,27 +29,13 @@ class Breeze::Requests::ShowPage < BreezeLayout
 
   def render_session_info
     req.session.as_h.each do |key, value|
-      div class: "mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5" do
-        dt class: "text-sm leading-5 font-medium text-gray-500" do
-          text "Session #{key}"
-        end
-        dd class: "mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2" do
-          text value.as_s
-        end
-      end
+      mount Breeze::DescriptionListRow.new("Session #{key}", value.as_s)
     end
   end
 
   def render_header_info
     req.headers.as_h.each do |key, value|
-      div class: "mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5" do
-        dt class: "text-sm leading-5 font-medium text-gray-500" do
-          text "Header #{key}"
-        end
-        dd class: "mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2" do
-          text value[0].as_s
-        end
-      end
+      mount Breeze::DescriptionListRow.new("Header #{key}", value[0].as_s)
     end
   end
 end
